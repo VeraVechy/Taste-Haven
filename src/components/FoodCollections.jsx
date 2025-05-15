20e8e70ed93a0c30ddfff5d5350d4da03a40e6ba
Vera <153821907+VeraVechy@users.noreply.github.com>
2025-05-15 14:29:42 +0100

    Update Header Navigation and Links

--- src/components/FoodCollections.jsx
+++ src/components/FoodCollections.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useCartStore from '../store/cartStore';
+import { toast } from 'react-hot-toast';
 import img1 from '../assets/images/pizza.png';
 import img2 from '../assets/images/shawama.png';
 import img3 from '../assets/images/fried-chicken.png';
@@ -10,101 +13,179 @@ import img8 from '../assets/images/fried-chips.png';
 
 import love from '../assets/images/loveIcon.png';
 import fullStar from '../assets/images/Fullstar.png';
-import star from '../assets/images/star.png'
-
+import star from '../assets/images/star.png';
 
 function FoodCollections() {
+  const navigate = useNavigate();
+  const addItem = useCartStore((state) => state.addItem);
 
-    const foodCategories = [
-        {pic: img1, caption: 'Pepperoni Pizza'},
-        {pic: img2, caption: 'Patato Chip'},
-        {pic: img3, caption: 'Cheese Burger'},
-        {pic: img4, caption: 'Grilled Chicken'}
-    ];
-    const foodCategories2 = [
-        {pic: img5, caption: 'Grilled Chicken'},
-        {pic: img6, caption: 'Grilled Chicken'},
-        {pic: img7, caption: 'Grilled Chicken'},
-        {pic: img8, caption: 'Grilled Chicken'}
-    ]
-
-    const rateFood = {
-        heart: love,
-        fullRate: fullStar,
-        noRate: star
+  const foodCategories = [
+    {
+      id: 1,
+      pic: img1,
+      name: 'Pepperoni Pizza',
+      price: 30.52,
+      originalPrice: 38.52,
+      category: 'pizza'
+    },
+    {
+      id: 2,
+      pic: img2,
+      name: 'Beef Shawarma',
+      price: 25.99,
+      originalPrice: 32.99,
+      category: 'shawarma'
+    },
+    {
+      id: 3,
+      pic: img3,
+      name: 'Fried Chicken',
+      price: 28.99,
+      originalPrice: 35.99,
+      category: 'chicken'
+    },
+    {
+      id: 4,
+      pic: img4,
+      name: 'French Fries',
+      price: 12.99,
+      originalPrice: 15.99,
+      category: 'sides'
     }
+  ];
+
+  const foodCategories2 = [
+    {
+      id: 5,
+      pic: img5,
+      name: 'Cheese Burger',
+      price: 22.99,
+      originalPrice: 27.99,
+      category: 'burger'
+    },
+    {
+      id: 6,
+      pic: img6,
+      name: 'Chicken Biryani',
+      price: 32.99,
+      originalPrice: 39.99,
+      category: 'rice'
+    },
+    {
+      id: 7,
+      pic: img7,
+      name: 'Pasta Alfredo',
+      price: 24.99,
+      originalPrice: 29.99,
+      category: 'pasta'
+    },
+    {
+      id: 8,
+      pic: img8,
+      name: 'Loaded Fries',
+      price: 15.99,
+      originalPrice: 18.99,
+      category: 'sides'
+    }
+  ];
+
+  const handleAddToCart = (item) => {
+    addItem({
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      image: item.pic
+    });
+    toast.success(`${item.name} added to cart!`);
+  };
+
+  const rateFood = {
+    heart: love,
+    fullRate: fullStar,
+    noRate: star
+  };
+
   return (
     <>
-    <div className="container-fluid">
+      <div className="container-fluid">
         <div className="container">
-            <div className="text-center mb-4">
-                <p className="text-success">CRISPY, EVERY BITE TASTE</p>
-                <h4>POPULAR FOOD ITEMS</h4>
-            </div>
+          <div className="text-center mb-4">
+            <p className="text-success">CRISPY, EVERY BITE TASTE</p>
+            <h4>POPULAR FOOD ITEMS</h4>
+          </div>
 
-            <div className="row">
-
-                {foodCategories.map((foodCategory, index) =>(
-                    <div className="food-items col-lg-3 col-md-6 col-sm-6" key={index}>
-                        <div className="Fastfood-btn">
-                            <div className="fastFoods">
-                                <div className="foods mx-auto">
-                                    <img src={foodCategory.pic} alt="Food items"  className='img-fluid'/>
-                                </div>
-                                <div className="love">
-                                    <img src={rateFood.heart} alt="Love" className='img-fluid'/>
-                                </div>
-                            </div>
-                            <button style={{width: '10rem'}} className='btn btn-secondary mx-auto display-btn'>Add To Chart</button>
-                        </div>
-
-
-                        <div className="text pt-3">
-                            <span>Beef Shawarma</span>
-
-                            <div className="img-rating">
-                                <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
-                                <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
-                                <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
-                                <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
-                                <img src={rateFood.noRate} alt="Ratings" className='img-fluid'/>
-                            </div>
-                            <p>$30.52 <span style={{textDecoration: 'line-through', fontSize: '12px'}}>$38.52</span></p>
-                        </div>
+          <div className="row">
+            {foodCategories.map((foodCategory) => (
+              <div className="food-items col-lg-3 col-md-6 col-sm-6" key={foodCategory.id}>
+                <div className="Fastfood-btn">
+                  <div className="fastFoods">
+                    <div className="foods mx-auto">
+                      <img src={foodCategory.pic} alt={foodCategory.name} className='img-fluid'/>
                     </div>
-                ))}
-            </div>
-            <div className="row my-2">
-                {foodCategories2.map((foodCategory, index) =>(
-                    <div className="food-items col-lg-3 col-md-6 col-sm-6" key={index}>
-                        <div className="Fastfood-btn">
-                            <div className="fastFoods">
-                                <img src={foodCategory.pic} alt=""  className='img-fluid'/>
-                                <div className="love">
-                                    <img src={rateFood.heart} alt="" />
-                                </div>
-                            </div>
-                            <button style={{width: '10rem'}} className='btn btn-secondary mx-auto display-btn'>Add To Chart</button>
-                        </div>
-                        <div className="text pt-3">
-                            <span>Beef Shawarma</span>
-
-                            <div className="img-rating">
-                                <img src={rateFood.fullRate} alt="Ratings" />
-                                <img src={rateFood.fullRate} alt="Ratings" />
-                                <img src={rateFood.fullRate} alt="Ratings" />
-                                <img src={rateFood.fullRate} alt="Ratings" />
-                                <img src={rateFood.noRate} alt="Ratings" />
-                            </div>
-                            <p>$30.52 <span style={{textDecoration: 'line-through', fontSize: '12px'}}>$38.52</span></p>
-                        </div>
+                    <div className="love">
+                      <img src={rateFood.heart} alt="Love" className='img-fluid'/>
                     </div>
-                ))}
-            </div>
+                  </div>
+                  <button 
+                    style={{width: '10rem'}} 
+                    className='btn btn-secondary mx-auto display-btn'
+                    onClick={() => handleAddToCart(foodCategory)}
+                  >
+                    Add To Cart
+                  </button>
+                </div>
+
+                <div className="text pt-3">
+                  <span>{foodCategory.name}</span>
+                  <div className="img-rating">
+                    <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
+                    <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
+                    <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
+                    <img src={rateFood.fullRate} alt="Ratings" className='img-fluid'/>
+                    <img src={rateFood.noRate} alt="Ratings" className='img-fluid'/>
+                  </div>
+                  <p>${foodCategory.price} <span style={{textDecoration: 'line-through', fontSize: '12px'}}>${foodCategory.originalPrice}</span></p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="row my-2">
+            {foodCategories2.map((foodCategory) => (
+              <div className="food-items col-lg-3 col-md-6 col-sm-6" key={foodCategory.id}>
+                <div className="Fastfood-btn">
+                  <div className="fastFoods">
+                    <img src={foodCategory.pic} alt={foodCategory.name} className='img-fluid'/>
+                    <div className="love">
+                      <img src={rateFood.heart} alt="Favorite" />
+                    </div>
+                  </div>
+                  <button 
+                    style={{width: '10rem'}} 
+                    className='btn btn-secondary mx-auto display-btn'
+                    onClick={() => handleAddToCart(foodCategory)}
+                  >
+                    Add To Cart
+                  </button>
+                </div>
+                <div className="text pt-3">
+                  <span>{foodCategory.name}</span>
+                  <div className="img-rating">
+                    <img src={rateFood.fullRate} alt="Ratings" />
+                    <img src={rateFood.fullRate} alt="Ratings" />
+                    <img src={rateFood.fullRate} alt="Ratings" />
+                    <img src={rateFood.fullRate} alt="Ratings" />
+                    <img src={rateFood.noRate} alt="Ratings" />
+                  </div>
+                  <p>${foodCategory.price} <span style={{textDecoration: 'line-through', fontSize: '12px'}}>${foodCategory.originalPrice}</span></p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default FoodCollections
+export default FoodCollections;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,70 +1,99 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuthStore from '../store/authStore';
+import useCartStore from '../store/cartStore';
 import logo from '../assets/TasteHaven_Cafe_logo.png';
 import instagram from '../assets/instagram.png';
 import facebook from '../assets/facebook.png';
 import call from '../assets/call.png';
 import search from '../assets/search.png';
 
-
 function Header() {
+  const { user } = useAuthStore();
+  const cartItems = useCartStore((state) => state.items);
+  const navigate = useNavigate();
+
+  const cartItemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <>
-    <div className="container-fluid">
-      <div className="headline-info row">
-        <div className="col-4 adding-border pt-3">
-          <p><span className='text-success'>100%</span> Secure delivery </p>
-        </div>
-        <div className="col-4 search adding-border1 pt-3">
-          <input type="search" placeholder='Search'/>
-          <img src={search} alt="search image" />
-        </div>
-        <div className="col-4 d-flex adding-border1 pt-3">
-          <div className="headline-images">
-            <div className="img-headline">
-              <img src={call} alt="Call Nuber 08167000077" />
-            </div>
-            <p>08167000077</p>
+      <div className="container-fluid">
+        <div className="headline-info row">
+          <div className="col-4 adding-border pt-3">
+            <p><span className='text-success'>100%</span> Secure delivery </p>
           </div>
-          <div className="headline-images">
-            <div className="img-headlie">
-              <img src={facebook} alt="Facebook" />
-            </div>
-            <p>Taste_heaven</p>
+          <div className="col-4 search adding-border1 pt-3">
+            <input type="search" placeholder='Search'/>
+            <img src={search} alt="search image" />
           </div>
-          <div className="headline-images">
-            <div className="img-headline">
-              <img src={instagram} alt="Instagram" />
+          <div className="col-4 d-flex adding-border1 pt-3">
+            <div className="headline-images">
+              <div className="img-headline">
+                <img src={call} alt="Call Number 08167000077" />
+              </div>
+              <p>08167000077</p>
             </div>
-            <p>Taste_heaven</p>
+            <div className="headline-images">
+              <div className="img-headlie">
+                <img src={facebook} alt="Facebook" />
+              </div>
+              <p>Taste_heaven</p>
+            </div>
+            <div className="headline-images">
+              <div className="img-headline">
+                <img src={instagram} alt="Instagram" />
+              </div>
+              <p>Taste_heaven</p>
+            </div>
           </div>
         </div>
-      </div>
-      {/* <hr /> */}
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      
-      <div className="container">
-        <a className="navbar-brand" href="/"><img src={logo} alt="Taste Heaven Logo" style={{width:'3rem'}}/></a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav mx-auto">
-            <a className="nav-link active text-success" aria-current="page" href="/">Home</a>
-            <a className="nav-link" href="/service">Shop</a>
-            <a className="nav-link" href="/tracking">About</a>
-            <a className="nav-link" href="/contact">Contact</a>
-            <a className="nav-link" href="/contact">Cart</a>
-        </div>
-        <button className="btn border border-success bg-white text-success me-2">Register</button>
-        <button className="btn bg-success text-white">Login</button>
-        </div>
-      </div>
-    </nav>
-    </div>
 
-
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              <img src={logo} alt="Taste Heaven Logo" style={{width:'3rem'}}/>
+            </Link>
+            <button 
+              className="navbar-toggler" 
+              type="button" 
+              data-bs-toggle="collapse" 
+              data-bs-target="#navbarNavAltMarkup" 
+              aria-controls="navbarNavAltMarkup" 
+              aria-expanded="false" 
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav mx-auto">
+                <Link className="nav-link active text-success" to="/">Home</Link>
+                <Link className="nav-link" to="/menu">Menu</Link>
+                <Link className="nav-link" to="/reservations">Reservations</Link>
+                <Link className="nav-link position-relative" to="/cart">
+                  Cart
+                  {cartItemCount > 0 && (
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                      {cartItemCount}
+                    </span>
+                  )}
+                </Link>
+              </div>
+              {user ? (
+                <div className="d-flex gap-2">
+                  <Link to="/profile" className="btn border border-success bg-white text-success">Profile</Link>
+                </div>
+              ) : (
+                <div className="d-flex gap-2">
+                  <Link to="/auth" className="btn border border-success bg-white text-success">Sign In</Link>
+                  <Link to="/auth" className="btn bg-success text-white">Sign Up</Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </nav>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
